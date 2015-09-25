1f84d8b4d1aa884e5b459e6ff34060ff1879bdf4
Felipe Skinner <felipeskinner@gmail.com>
2015-09-25 16:09:35 -0300

    Logica basica do app

---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+import {setEntries, next, vote, INITIAL_STATE} from './core';
+
+export default function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'SET_ENTRIES':
+      return setEntries(state, action.entries);
+
+    case 'NEXT':
+      return next(state);
+
+    case 'VOTE':
+      return vote(state, action.entry);
+
+    default:
+      return state;
+
+  }
+  // Figure out which function to call and call it
+}
